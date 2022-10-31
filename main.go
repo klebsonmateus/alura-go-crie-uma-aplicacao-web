13318e0d0a2019d0fdf00d354f83950dc2f42150
Klebson Mateus <klebsonbm@gmail.com>
2022-10-31 17:33:05 -0300

    Finalizando Capitulo 01: Criando struct de produtos

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	produtos := []Produto{
+		{Nome: "Camiseta",Descricao: "Azul, bem bonita",Preco: 39 ,Quantidade: 5},
+		{Nome: "Tênis",Descricao: "Confortável",Preco: 89 ,Quantidade: 3},
+		{Nome: "Fone",Descricao: "Muito bom",Preco: 59 ,Quantidade: 2},
+		{Nome: "Produto Novo",Descricao: "Muito legal",Preco: 1.99 ,Quantidade: 1},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
